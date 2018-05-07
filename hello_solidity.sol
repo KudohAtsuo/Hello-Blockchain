e910a9e9e363f20c1df98badceb783e27ad14b72
KudohAtsuo <atsuokudoh@gmail.com>
2018-05-08 08:01:17 +0900

    add vew modifier

--- hello_solidity.sol
+++ hello_solidity.sol
@@ -1,30 +1,30 @@
-pragma solidity ^0.4.23;
-///@ title Test contract :)
-///@ dev Comment represents greeting message. Owener may not be necessary.
+pragma solidity ^0.4.2;
+// Test contract :)
+// Comment represents greeting message. Owener may not be necessary.
 contract HelloSolidity{
   string public comment1;
   address public owner;
 
-///@ dev Cnstructor
+// Cnstructor
 function HelloSolidity(string _msg1){
     comment1=_msg1;
     owner=msg.sender;
   }
 
-///@ dev Modifier onlyOwner.
+// Modifier onlyOwner.
 modifier onlyOwner(){
   require(msg.sender==owner);
   _;
 }
 
-///@ dev Setter function for your message.
-///@ pram The message of your choice!
+// Setter function for your message.
+// The message of your choice!
 function setMessage(string _message) public onlyOwner{
   comment1=_message;
 }
 
-///@ dev Getter function for your message.
-function getMessage() public {
+// Getter function for your message.
+function getMessage() view public returns(string _message){
   return comment1;
 }
 }
